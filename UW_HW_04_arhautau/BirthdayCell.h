6904b35698f1c6ca8b5f7c9befdd0a3f429166a3
Andrew Hautau <arhautau@gmail.com>
2015-05-08 00:45:20 -0700

    first commit

---
+++ UW_HW_04_arhautau/BirthdayCell.h
@@ -0,0 +1,13 @@
+//
+//  BirthdayCell.h
+//  UW_HW_04_arhautau
+//
+//  Created by Andrew Hautau on 5/7/15.
+//  Copyright (c) 2015 Andrew Hautau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BirthdayCell : UITableViewCell
+
+@end
---
+++ UW_HW_04_arhautau/BirthdayCell.m
@@ -0,0 +1,23 @@
+//
+//  BirthdayCell.m
+//  UW_HW_04_arhautau
+//
+//  Created by Andrew Hautau on 5/7/15.
+//  Copyright (c) 2015 Andrew Hautau. All rights reserved.
+//
+
+#import "BirthdayCell.h"
+
+@implementation BirthdayCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
