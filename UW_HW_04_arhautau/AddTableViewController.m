8354ca0dc71044ac12e2ba730cf6dcf88bc5db15
Andrew Hautau <arhautau@gmail.com>
2015-05-10 17:00:19 -0700

    birthdays in ascending order. insert rows and remove rows with animations. remove bug with row reuse. add pragma marks.

--- UW_HW_04_arhautau/AddTableViewController.m
+++ UW_HW_04_arhautau/AddTableViewController.m
@@ -26,17 +26,24 @@
     [self setupDatePicker];
     [self updateBirthdayField];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void) updateBirthdayField {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle: NSDateFormatterLongStyle];
+    [self.birthdayField setText: [formatter stringFromDate: [self.datePicker date]]];
+}
+
+-(ListViewController*) listViewController {
+    return (ListViewController *) [[self navigationController] presentingViewController];
+}
+
+# pragma mark Setup UIDatePicker
 
 -(void) setupDatePicker {
     [self.datePicker setDatePickerMode: UIDatePickerModeDate];
@@ -54,21 +61,12 @@
     [self.datePicker setDate: firstOfJanuaryOfPreviousYear];
 }
 
+# pragma mark IBActions
+
 - (IBAction)dateValueChanged:(id)sender {
     [self updateBirthdayField];
 }
 
--(void) updateBirthdayField {
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateStyle: NSDateFormatterLongStyle];
-    [self.birthdayField setText: [formatter stringFromDate: [self.datePicker date]]];
-}
-
--(ListViewController*) listViewController {
-    return (ListViewController *) [[self navigationController] presentingViewController];
-}
-
-# pragma mark — IB Actions
 - (IBAction)tappedCancel:(id)sender {
     ListViewController *listVC = [self listViewController];
     [listVC dismissViewControllerAnimated:YES completion:^{
--- UW_HW_04_arhautau/ListViewController.m
+++ UW_HW_04_arhautau/ListViewController.m
@@ -24,8 +24,6 @@
     [self.tableView setDelegate:self];
     
     [self prepopulateBirthdays];
-    
-    [self.tableView reloadData];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -33,6 +31,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+# pragma mark IBActions
 
 - (IBAction)addButtonTapped:(id)sender {
     
@@ -47,15 +47,14 @@
                        animated: YES completion:nil];
 }
 
+# pragma mark UITableViewDataSource methods
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
     return [[self birthdays] count];
 }
 
@@ -68,36 +67,83 @@
     if (cell == nil) {
         NSArray *nibObjects = [[NSBundle mainBundle] loadNibNamed:cellID owner:self options:nil];
         cell = (BirthdayCell *) [nibObjects firstObject];
-        
-        NSDictionary *birthday = [[self birthdays] objectAtIndex: [indexPath row]];
-        
-        [[cell nameLabel] setText: [birthday valueForKey:@"name"]];
-        [[cell dateLabel] setText: [birthday valueForKey:@"date"]];
-        [[cell daysLabel] setText: [birthday valueForKey:@"days"]];
     }
+    
+    NSDictionary *birthday = [[self birthsdaysAscending] objectAtIndex: [indexPath row]];
+    
+    [[cell nameLabel] setText: [birthday valueForKey:@"name"]];
+    [[cell dateLabel] setText: [birthday valueForKey:@"date"]];
+    [[cell daysLabel] setText: [birthday valueForKey:@"days"]];
     
     return cell;
 }
 
--(void) prepopulateBirthdays {
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateStyle: NSDateFormatterMediumStyle];
-    [formatter setDateFormat: @"dd-MM-yyyy"];
-    NSString *originalRelease = @"25-05-1975";
-    NSDate *starWarsDate = [formatter dateFromString: originalRelease];
-    
-    NSDictionary *firstBirthday = [self birthdayDictionaryWithDate: starWarsDate AndName:@"Darth Vader"];
-    NSDictionary *secondBirthday = [self birthdayDictionaryWithDate: starWarsDate AndName:@"Darth Vader"];
-    
-    [self setBirthdays: @[firstBirthday, secondBirthday]];
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
 }
 
+# pragma mark Deleting rows
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSMutableArray *ascendingBirthdays = [NSMutableArray arrayWithArray: [self birthsdaysAscending]];
+        NSUInteger row = [indexPath row];
+        NSLog(@"foo");
+        [ascendingBirthdays removeObjectAtIndex: row];
+        [self setBirthdays: ascendingBirthdays];
+        [self.tableView deleteRowsAtIndexPaths: @[indexPath] withRowAnimation: UITableViewRowAnimationFade];
+        
+    }
+}
+
+# pragma mark Inserting rows
 
 -(void) addBirthdayWithDate:(NSDate *)date AndWithName:(NSString *)name {
     NSDictionary *birthday = [self birthdayDictionaryWithDate: date AndName: name];
+    
     [self setBirthdays: [self.birthdays arrayByAddingObject: birthday]];
-    [self.tableView reloadData];
+    // insert row
+    NSUInteger row = [[self birthsdaysAscending] indexOfObject: birthday];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow: row inSection: 0];
+    
+    [self.tableView insertRowsAtIndexPaths: @[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
+
+# pragma mark Prepopulating rows
+
+-(void) prepopulateBirthdays {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle: NSDateFormatterMediumStyle];
+    
+    [formatter setDateFormat: @"dd-MM-yyyy"];
+    NSString *originalRelease = @"25-05-1975";
+    NSString *lucasBirthday = @"14-05-1944";
+    NSDate *starWarsDate = [formatter dateFromString: originalRelease];
+    NSDate *lucasBirthdayDate = [formatter dateFromString: lucasBirthday];
+    
+    NSDictionary *firstBirthday = [self birthdayDictionaryWithDate: starWarsDate AndName:@"Darth Vader"];
+    NSDictionary *secondBirthday = [self birthdayDictionaryWithDate: lucasBirthdayDate AndName:@"George Lucas"];
+    [self setBirthdays: @[firstBirthday, secondBirthday]];
+    
+    NSIndexPath *firstRow = [NSIndexPath indexPathForRow:0 inSection:0];
+    NSIndexPath *secondRow = [NSIndexPath indexPathForRow:0 inSection:0];
+
+    [self.tableView insertRowsAtIndexPaths: @[firstRow, secondRow] withRowAnimation:UITableViewRowAnimationLeft];
+}
+
+# pragma mark Ascending birthdays
+
+-(NSArray*) birthsdaysAscending {
+    NSArray *birthdays = [self.birthdays sortedArrayWithOptions: NSSortConcurrent usingComparator:^NSComparisonResult(id obj1, id obj2) {
+        NSUInteger days1 = [[obj1 valueForKey: @"days"] integerValue];
+        NSUInteger days2 = [[obj2 valueForKey: @"days"] integerValue];
+        BOOL ascending = days2 < days1;
+        return ascending;
+    }];
+    return birthdays;
+}
+
+# pragma mark Generating birthday NSDictionaries
 
 -(NSDictionary*) birthdayDictionaryWithDate:(NSDate*)date AndName:(NSString*)name {
     NSString *daysString = [self daysTillNextBirthdayWithDate: date];
@@ -113,29 +159,29 @@
 }
 
 -(NSString*) daysTillNextBirthdayWithDate: (NSDate*) date {
-    // date String
+
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     [formatter setDateStyle:NSDateFormatterMediumStyle];
-    
-    NSString *dateString = [formatter stringFromDate:date];
     
     // get date day and month
     [formatter setDateFormat: @"dd-MM"];
     NSString *dateDayAndMonth = [formatter stringFromDate:date];
     
-    // get today's date set at midnight
+    // get today's date set at 12AM
     [formatter setDateFormat: @"dd-MM-yyyy"];
     NSString *todayString = [formatter stringFromDate: [NSDate date]];
     NSDate *today = [formatter dateFromString:todayString];
     
+    // get today's year
     [formatter setDateFormat: @"yyyy"];
     NSString *todayYear = [formatter stringFromDate: today];
     
-    // date with this year
+    // date with today's year
     [formatter setDateFormat: @"dd-MM-yyyy"];
     NSDate *dateWithCurrentYear = [formatter dateFromString: [[dateDayAndMonth stringByAppendingString:@"-"] stringByAppendingString:todayYear]];
     
     NSDate *nextBirthday = dateWithCurrentYear;
+    
     
     if ([today compare:dateWithCurrentYear] == NSOrderedDescending) {
         // the birthday has already happened this year.
